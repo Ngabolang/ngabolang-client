5d0d2a7d3691c4a43000a05e66bd2b02dd52e9c8
Jati Suryo <jati.suryo@gmail.com>
2023-07-06 07:28:14 +0700

    feat : last push before merge

--- client-customer/src/views/RegisterCustomerView.jsx
+++ client-customer/src/views/RegisterCustomerView.jsx
@@ -125,7 +125,7 @@ function RegisterCustomerView() {
                       onChange={handleForm}
                       name="password"
                       type="password"
-                      placeholder="Enter your password"
+                      placeholder="Masukan kata sandi anda disini"
                       className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                     />
                   </div>
@@ -172,7 +172,7 @@ function RegisterCustomerView() {
                   <textarea
                     onChange={handleForm}
                     className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                    placeholder="Insert menu description here"
+                    placeholder="Masukan alamat anda disini"
                     rows="4"
                     name="address"
                   ></textarea>
